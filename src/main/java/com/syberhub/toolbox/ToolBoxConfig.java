82aa295132c2004cbdaf7d8185c832302fa62371
syam <syamkumar911@gmail.com>
2021-05-09 02:30:37 +0530

    read properties from heroku Config Vars

--- src/main/java/com/syberhub/toolbox/ToolBoxConfig.java
+++ src/main/java/com/syberhub/toolbox/ToolBoxConfig.java
@@ -1,28 +1,23 @@
 package com.syberhub.toolbox;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 public class ToolBoxConfig {
-	private static Properties prop;
-	static {
-		InputStream is = null;
-		try {
-			prop = new Properties();
-			is = ClassLoader.class.getResourceAsStream("/application.properties");
-			prop.load(is);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	private static Properties prop;
+//	static {
+//		InputStream is = null;
+//		try {
+//			prop = new Properties();
+//			is = ClassLoader.class.getResourceAsStream("/application.properties");
+//			prop.load(is);
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	public static String getProperty(String key) {
-//		return System.getenv(key);
-		return prop.getProperty(key);
+		return System.getenv(key); // for heroku
+		// return prop.getProperty(key); // local system
 	}
 
 	public static boolean getBooleanProperty(String key) {
